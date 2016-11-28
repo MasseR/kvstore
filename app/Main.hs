d9d192010af52ff64aa2ef2d2dd9cd24a2393472
Mats Rauhala <mats.rauhala@iki.fi>
2016-11-28 23:04:56 +0200

    Get keys

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,9 @@
 module Main where
 
+import Database.SQLite.Simple
 import Lib
 
 main :: IO ()
-main = startApp
+main = do
+    conn <- open "test.db"
+    startApp conn
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DataKinds       #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeOperators   #-}
+{-# LANGUAGE OverloadedStrings   #-}
 module Lib
     ( startApp
     ) where
@@ -9,31 +10,37 @@ import Data.Aeson
 import Data.Aeson.TH
 import Network.Wai
 import Network.Wai.Handler.Warp
+import Data.Text (Text)
+import Database.SQLite.Simple
 import Servant
+import Control.Monad.Trans (MonadIO, liftIO)
 
-data User = User
-  { userId        :: Int
-  , userFirstName :: String
-  , userLastName  :: String
-  } deriving (Eq, Show)
+data KeyVal = KeyVal
+            { key :: Key
+            , value :: Text
+            } deriving (Eq, Show)
 
-$(deriveJSON defaultOptions ''User)
+type Key = Text
+$(deriveJSON defaultOptions ''KeyVal)
 
-type API = "users" :> Get '[JSON] [User]
 
-startApp :: IO ()
-startApp = run 8080 app
+type API = "key" :> Capture "key" Key :> Get '[JSON] [KeyVal]
 
-app :: Application
-app = serve api server
+startApp :: Connection -> IO ()
+startApp conn = run 8080 (app conn)
+
+app :: Connection -> Application
+app conn = serve api (server conn)
 
 api :: Proxy API
 api = Proxy
 
-server :: Server API
-server = return users
+getter :: MonadIO m => Connection -> Key -> m [KeyVal]
+getter conn key = do
+    vals <- liftIO $ queryNamed conn "select key, name from keys where name=:name" [":name" := key]
+    liftIO $ print vals
+    return [KeyVal k v | (k,v) <- vals]
 
-users :: [User]
-users = [ User 1 "Isaac" "Newton"
-        , User 2 "Albert" "Einstein"
-        ]
+server :: Connection -> Server API
+server conn = getter conn
+
